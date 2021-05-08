b88d8d423af25fcf002e1a6d3cc23f5a68dce7ce
Sifrisky <astrid.amg@gmail.com>
2021-05-08 11:34:30 -0300

    cart-shopping-funcional

--- src/components/Products.js
+++ src/components/Products.js
@@ -9,25 +9,19 @@ export default class Products extends Component {
                     {this.props.products.map((product) =>(
                         <li key={product._id}>
                         <div className='product'>
-                            <a href={'# + product._id'}>
+                            <a href={'#' + product._id}>
                                 <img src={product.image} alt= {product.title}></img>
-                            <p>
-                                {product.title}
-                            </p>
+                            <p>{product.title}</p>
                             </a>
                             <div className='product-price'>
-                                <div>
-                                    {formatCurrency(product.price)}
-                                </div>
-                                <button className='button-primary'>
-                                    Add To Cart
-                                </button>
+                                <div>{formatCurrency(product.price)}</div>
+                                <button onClick={() => this.props.addToCart(product)} className='button-primary'>Add To Cart</button>
                             </div>
                         </div>
                         </li>
                     ))}
                 </ul>
             </div>
-        )
+        );
     }
 }
